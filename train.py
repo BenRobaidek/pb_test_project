d41f08e5b1e2489ea04b5c79c9d769965e3d0938
Ben Robaidek <brobaidek@gmail.com>
2018-08-19 15:11:57 -0700

    evaluate

--- train.py
+++ train.py
@@ -85,6 +85,7 @@ def evaluate(data_iter, model, TEXT, LABEL):
         _, preds = torch.max(preds, 1)
         print('preds:', preds.data)
         print('targets:', target.data)
+        print('sum:', preds.data.eq(target.data).sum())
         corrects += preds.data.eq(target.data).sum()
     return 100 * corrects / len(data_iter.dataset)
 
