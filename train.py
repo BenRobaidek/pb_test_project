12c0383d7e708217fa6feec083eb6659bcb85130
Ben Robaidek <brobaidek@gmail.com>
2018-08-27 10:47:57 -0700

    hyperparams

--- train.py
+++ train.py
@@ -144,12 +144,12 @@ def main():
     test_path = 'test.tsv'
 
     # hyperparams
-    hidden_size = 64
+    hidden_size = 32
     num_classes = 2
-    num_layers = 2
-    num_dir = 2
+    num_layers = 1
+    num_dir = 1
     batch_size = 8
-    emb_dim = 300
+    emb_dim = 50
     dropout = .8
     net_type = 'lstm'
     embfix=False
